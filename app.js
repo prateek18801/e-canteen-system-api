88f87a07cbcfcb9652be7334781289984460209e
Prateek Chaurasia <prateek18801@gmail.com>
2023-02-10 01:11:19 +0530

    use admin routes

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 
+const adminRouter = require('./api/routes/admin');
+
 require('./api/utils/db').connect();
 
 const app = express();
@@ -10,6 +12,8 @@ app.use(express.urlencoded({ extended: false }));
 app.get('/', (req, res) => {
     res.status(200).send('Hello World');
 });
+
+app.use('/admin', adminRouter);
 
 app.listen(process.env.PORT, () => {
     console.log(`server running on PORT:${process.env.PORT}`);
