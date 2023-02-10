b3f3ca0ca5dceba14d905c139af437ff17f14401
Prateek Chaurasia <prateek18801@gmail.com>
2023-02-10 13:25:42 +0530

    update admin routes

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -1,9 +1,14 @@
 const router = require('express').Router();
 
+const userController = require('../controllers/user');
 const adminController = require('../controllers/admin');
 
-router.get('/product/:id?', adminController.getProduct);
-router.post('/product/:id?', adminController.postProduct);
-router.delete('/product/:id', adminController.deleteProduct);
+// product routes
+router.get('/v1/product/:id?', userController.getProduct);
+router.post('/v1/product/:id?', adminController.postProduct);
+router.delete('/v1/product/:id', adminController.deleteProduct);
+
+router.get('/v1/available', userController.getAvailableProduct);
+router.get('/v1/available/toggle/:id', adminController.toggleAvailableProduct);
 
 module.exports = router;
