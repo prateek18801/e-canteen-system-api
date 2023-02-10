a6fc82ec7875954ef22ba4e18c2400329658df22
Prateek Chaurasia <prateek18801@gmail.com>
2023-02-10 17:50:19 +0530

    use auth routes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const userRouter = require('./api/routes/user');
+const authRouter = require('./api/routes/auth');
 const adminRouter = require('./api/routes/admin');
 
 require('./api/utils/db').connect();
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/api', userRouter);
+app.use('/auth', authRouter);
 app.use('/admin', adminRouter);
 
 app.listen(process.env.PORT, () => {
