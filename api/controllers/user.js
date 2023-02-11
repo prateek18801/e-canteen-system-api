517a4e59063be57fefd30381a0b6be3e1880a8e1
Prateek Chaurasia <prateek18801@gmail.com>
2023-02-11 23:11:40 +0530

    Add cart controllers

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,11 +1,14 @@
 const Product = require('../models/product');
+const User = require('../models/user');
+
+// product controllers
 
 exports.getProduct = async (req, res, next) => {
     const { id } = req.params;
     try {
         if (id) {
             const product = await Product.findById(id).lean();
-            
+
             if (!product) {
                 return res.status(400).json({
                     ok: false,
@@ -40,6 +43,51 @@ exports.getAvailableProduct = async (req, res, next) => {
             ok: true,
             message: 'success',
             data: products
+        });
+    } catch (err) {
+        next(err);
+    }
+}
+
+
+// cart controllers
+
+exports.getCart = async (req, res, next) => {
+    try {
+        // req.user.id = '63e7324607dd76d4bc12f9c2';
+        user_id = '63e7324607dd76d4bc12f9c2';
+        const user = await User.findById(user_id).populate('cart');
+        return res.status(200).json({
+            ok: true,
+            message: 'found',
+            data: user.cart
+        });
+    } catch (err) {
+        next(err);
+    }
+}
+
+exports.addToCart = async (req, res, next) => {
+    const id = req.params.id;
+    try {
+        user_id = '63e7324607dd76d4bc12f9c2';
+        const user = await User.findById(user_id);
+        // user.updateCart(id);
+
+        const idx = user.cart.findIndex(el => el.product_id.toString() === id);
+        if (idx > -1) {
+            user.cart[idx].qty++;
+        } else {
+            const data = { product_id: id, qty: 1 };
+            user.cart.push(data);
+        }
+
+        const updated = await user.save();
+
+        return res.status(200).json({
+            ok: true,
+            message: 'updated',
+            data: updated.cart
         });
     } catch (err) {
         next(err);
