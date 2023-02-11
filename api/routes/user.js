abd1640184f0c93b083d58bbaa0e405716f553dd
Prateek Chaurasia <prateek18801@gmail.com>
2023-02-11 23:13:24 +0530

    Add cart routes

--- api/routes/user.js
+++ api/routes/user.js
@@ -6,4 +6,9 @@ const userController = require('../controllers/user');
 router.get('/v1/product/:id?', userController.getProduct);
 router.get('/v1/available', userController.getAvailableProduct);
 
+// cart routes
+router.get('/v1/cart', userController.getCart);
+router.post('/v1/cart', userController.postCart);
+router.get('/v1/cart/add/:id', userController.addToCart);
+
 module.exports = router;
