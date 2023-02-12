e3596524c75d27a6f2336f930a53e9a98d0cc6de
Prateek Chaurasia <prateek18801@gmail.com>
2023-02-12 20:08:13 +0530

    Add authorization Update category in product route

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -2,13 +2,14 @@ const router = require('express').Router();
 
 const userController = require('../controllers/user');
 const adminController = require('../controllers/admin');
+const authorization = require('../middlewares/authorization');
 
 // product routes
-router.get('/v1/product/:id?', userController.getProduct);
-router.post('/v1/product/:id?', adminController.postProduct);
-router.delete('/v1/product/:id', adminController.deleteProduct);
+router.get('/v1/product/:category?', authorization('admin'), userController.getProduct);
+router.post('/v1/product/:id?', authorization('admin'), adminController.postProduct);
+router.delete('/v1/product/:id', authorization('admin'), adminController.deleteProduct);
 
-router.get('/v1/available', userController.getAvailableProduct);
-router.get('/v1/available/toggle/:id', adminController.toggleAvailableProduct);
+router.get('/v1/available', authorization('admin'), userController.getAvailableProduct);
+router.get('/v1/available/toggle/:id', authorization('admin'), adminController.toggleAvailableProduct);
 
 module.exports = router;
