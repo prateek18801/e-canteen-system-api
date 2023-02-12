eb0adb76342b7561446fd35467faeccef8160786
Prateek Chaurasia <prateek18801@gmail.com>
2023-02-12 09:48:42 +0530

    Enable cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const userRouter = require('./api/routes/user');
 const authRouter = require('./api/routes/auth');
@@ -8,6 +9,7 @@ require('./api/utils/db').connect();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
