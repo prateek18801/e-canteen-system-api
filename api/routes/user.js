383ab7af9ade4b86a0f127a55c6ef6f027208703
Prateek Chaurasia <prateek18801@gmail.com>
2023-02-12 19:59:15 +0530

    Add user authorization Update product category route

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,14 +1,16 @@
 const router = require('express').Router();
 
 const userController = require('../controllers/user');
+const authorization = require('../middlewares/authorization');
 
 // product routes
-router.get('/v1/product/:id?', userController.getProduct);
+router.get('/v1/product/:category?', userController.getProduct);
 router.get('/v1/available', userController.getAvailableProduct);
 
 // cart routes
-router.get('/v1/cart', userController.getCart);
-router.post('/v1/cart', userController.postCart);
-router.get('/v1/cart/add/:id', userController.addToCart);
+router.get('/v1/cart', authorization('user'), userController.getCart);
+router.post('/v1/cart', authorization('user'), userController.postCart);
+router.get('/v1/cart/add/:id', authorization('user'), userController.addToCart);
+
 
 module.exports = router;
