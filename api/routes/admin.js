afc85ab11e08125810eb6bca05ce874edf6b7047
Prateek Chaurasia <prateek18801@gmail.com>
2023-02-19 14:59:52 +0530

    Add get order route remove authorization for testing

--- api/routes/admin.js
+++ api/routes/admin.js
@@ -5,11 +5,13 @@ const adminController = require('../controllers/admin');
 const authorization = require('../middlewares/authorization');
 
 // product routes
-router.get('/v1/product/:category?', authorization('admin'), userController.getProduct);
-router.post('/v1/product/:id?', authorization('admin'), adminController.postProduct);
-router.delete('/v1/product/:id', authorization('admin'), adminController.deleteProduct);
+router.get('/v1/product/:category?', userController.getProduct);
+router.post('/v1/product/:id?', adminController.postProduct);
+router.delete('/v1/product/:id', adminController.deleteProduct);
 
-router.get('/v1/available', authorization('admin'), userController.getAvailableProduct);
-router.get('/v1/available/toggle/:id', authorization('admin'), adminController.toggleAvailableProduct);
+router.get('/v1/available', userController.getAvailableProduct);
+router.get('/v1/available/toggle/:id', adminController.toggleAvailableProduct);
+
+router.get('/v1/order/:status?', adminController.getOrder);
 
 module.exports = router;
